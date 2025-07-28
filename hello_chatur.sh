693ddbb6faf2a5527369d3383a17fb0263c65e94
karamsandhu2516 <karamsandhu2516@gmail.com>
2025-07-28 06:40:02 +0000

    added from local

---
+++ hello_chatur.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "this is about my fear and dream"
+
+echo "Fear: Devops is too hard to understand maybe beyod my caliber."
+
+echo "Dream: I want to be a devops eng. at any cost."
+
+echo "Hope: Need to chase my dream and work hard. I would get my dream job"
+
+mkdir -p hope
+
+echo "harwork is the key to success"
+
+echo "work-hard"
+
+echo "success need time and hardwork" > dream.txt
+
